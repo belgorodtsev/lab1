6cc3b2f99f2ddd11df893e8aeecc2000ddcbaf6d
belgorodtsev <32188418+belgorodtsev@users.noreply.github.com>
2017-09-23 22:39:42 +0300

    Update dzclassroom.cpp

--- dzclassroom.cpp
+++ dzclassroom.cpp
@@ -1,29 +1,6 @@
 #include <cmath>
 #include <iostream>
-
 int main()
-
-{   //1
-	double pi = 3.14;
-	std::cout << pi << std::endl;
-	//
-
-	//2
-	int a = 1, b = 13, c = 49;
-	std::cout << a << " " << b << " " << c << std::endl;
-	//
-
-	//3
-	char a1, a2;
-	std::cout << " write a symbol = " << std::endl;
-	std::cin >> a1;
-	std::cin >> a2;
-	std::cout << a << a1 << b << a2 << c << std::endl;
-	//
-
-	
-
+{  
 	return 0;
-
 }
-	
