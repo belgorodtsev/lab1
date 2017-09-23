c76cd18e49c5ef139a6da568a41fb31c402b52fa
belgorodtsev <32188418+belgorodtsev@users.noreply.github.com>
2017-09-23 21:54:28 +0300

    Update dzclassroom.cpp

--- dzclassroom.cpp
+++ dzclassroom.cpp
@@ -3,32 +3,32 @@
 
 int main()
 
-{   	////1
+{   //1
 	double pi = 3.14;
 	std::cout << pi << std::endl;
-	////////////
+	//
 
-	/////2
+	//2
 	int a = 1, b = 13, c = 49;
 	std::cout << a << " " << b << " " << c << std::endl;
-	//////////
+	//
 
-	/////3
+	//3
 	char a1, a2;
 	std::cout << " write a symbol = " << std::endl;
 	std::cin >> a1;
 	std::cin >> a2;
 	std::cout << a << a1 << b << a2 << c << std::endl;
-	//////
+	//
 
-	/////4
+	//4
 	double b1, b2, b3;
 	std::cout << " write a three numers = " << std::endl;	std::cin >> b1;
 	std::cin >> b2;
 	std::cin >> b3;
 	std::cout << b1 << " " << b2 << " " << b3 << std::endl;
-	//////////
-	//////5
+	//
+	//5
 	double a3, x, y;
 	std::cout << " write a = " << std::endl;
 	std::cin >> a3;
@@ -36,9 +36,9 @@ int main()
 	std::cout << " x = " << x << std::endl;
 	y = 3 * x * x * x + 4 * x * x - 11 * x + 1;
 	std::cout << " y = " << y << std::endl;
-	//////////
+	//
 
-	//////6
+	//6
 	double p, m, V;
 	std::cout << " write m = " << std::endl;
 	std::cin >> m;
@@ -46,9 +46,9 @@ int main()
 	std::cin >> V;
 	p = m / V;
 	std::cout << " p = " << p << std::endl;
-	/////
+	//
 
-	///////7
+	//7
 	double x1, a11, b11;
 	std::cout << " write a = " << std::endl;
 	std::cin >> a11;
@@ -56,9 +56,9 @@ int main()
 	std::cin >> b11;
 	x1 = -b11 / a11;
 	std::cout << " x = " << x1 << std::endl;
-	//////////
+	//
 
-	///////8
+	//8
 	double x11, x12, y11, y12, d1;
 	std::cout << " write x1" << std::endl;
 	std::cin >> x11;
@@ -70,9 +70,9 @@ int main()
 	std::cin >> y12;
 	d1 = sqrt(((x11 - x12) * (x11 - x12)) + ((y11 - y12) * (y11 - y12)));
 	std::cout << " distance =" << d1 << std::endl;
-	////////
+	//
 
-	////9
+	//9
 	double a9, b9, c9, h9, x9, p9;
 	std::cout << " Enter the title a = " << std::endl;
 	std::cin >> a9;
@@ -84,9 +84,9 @@ int main()
 	c9 = sqrt(x9 * x9 + h9 * h9);
 	p9 = a9 + b9 + 2 * c9;
 	std::cout << " Perimeter is p = " << p9 << std::endl;
-	/////////////
+	//
 
-	////10
+	//10
 	double r1, r2, s, s1, s2;
 	std::cout << " write inner radius r1 = " << std::endl;
 	std::cin >> r1;
@@ -96,9 +96,9 @@ int main()
 	s2 = pi * r2 * r2;
 	s = s2 - s1;
 	std::cout << " s = " << s << std::endl;
-	///////
+	//
 
-	////11
+	//11
 	double a111, Vk, sb;
 	std::cout << " write a = " << std::endl;
 	std::cin >> a111;
@@ -106,27 +106,25 @@ int main()
 	sb = 6 * a111 * a111;
 	std::cout << " cube volume V = " << Vk << std::endl;
 	std::cout << " Surface area = " << sb << std::endl;
-	//////
+	//
 
-	/////12
+	//12
 	int a12, p12;
 	std::cout << " write a = " << std::endl;
 	std::cin >> a12;
 	p12 = a12 * 4;
 	std::cout << " Perimeter is p = " << p12 << std::endl;
-	///////
+	//
 
-	//////13
+	//13
 	int r, d13;
 	std::cout << " Radius r = " << std::endl;
 	std::cin >> r;
 	d13 = r * 2;
 	std::cout << " Diameter d = " << d13 << std::endl;
-	/////////
+	//
 
 	return 0;
 
 }
-	
-
 	
